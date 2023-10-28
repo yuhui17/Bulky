5dae934fe1ceef436c126cb65bb10957d7e73443
yuhui17 <cheongyifui@gmail.com>
2023-10-29 01:38:03 +0800

    remove unecessary file

--- Bulky.Models/ApplicationUser.cs
+++ Bulky.Models/ApplicationUser.cs
@@ -19,8 +19,9 @@ namespace BulkyBook.Models
         public string? City { get; set; }
         public string? State { get; set; }
         public string? PostalCode { get; set; }
+        public int? CompanyId { get; set; }
         [ForeignKey("CompanyId")]
         [ValidateNever]
-        public int? CompanyId { get; set; }
+        public Company Company { get; set; }
     }
 }
